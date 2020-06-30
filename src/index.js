aef6a89618c97d2cb6be7a1eca60d15519fffaa8
kevinneyer <kevinneyer@gmail.com>
2020-06-30 11:08:32 -0400

    second  commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let tableBody = document.querySelector('#table-body') 
             let tableRow = document.createElement('tr')
             tableRow.innerHTML = `
-            <td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button>Edit Dog</button></td></tr>
+            <td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button id="${dog.id}">Edit Dog</button></td>
             `
             tableBody.append(tableRow)
         })
@@ -18,12 +18,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('click', function(e){
         if(e.target.innerTect = "Edit Dog"){
-         let dogForm = document.getElementById('dog-form')
-        console.log(e.target.parentElement.parentElement)
-        //get the input from the table row
-        //autopopulate it into the form
+          let id = e.target.id - 1
+          let tableBody = document.querySelector('#table-body') 
+
+          let dogForm = document.getElementById('dog-form')
+          dogForm.innerHTML = `
+            <input type="text" name="name" placeholder="dog's name" value="${tableBody.rows[id].cells[0].innerText}" />
+            <input type="text" name="breed" placeholder="dog's breed" value="${tableBody.rows[id].cells[1].innerText}" />
+            <input type="text" name="sex" placeholder="dog's sex" value="${tableBody.rows[id].cells[2].innerText}" />
+            <input type="submit" value="Submit" />
+          `
+        //√get the input from the table row
+        //√autopopulate it into the form
         }
-    })
+
+     })
+
+
+     document.addEventListener('click', function(e){
+        e.preventDefault()
+        if (e.target.innerText = "Submit"){
+            
+            console.log('success')
+        }
+     })
 
 
 })
