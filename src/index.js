1c09fea7d92442b923d44ac1f780ae47e62df73b
github username <github email address>
2020-06-30 19:26:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-let tableBody = document.getElementById("table-body")
-let formBody = document.getElementById('dog-form')
+let tBody = document.getElementById('table-body')
+let editForm = document.getElementById('dog-form')
 
-const fetchDogs = () =>{fetch ("http://localhost:3000/dogs")
-.then(resp => resp.json())
-.then(json => json.forEach(dog => postDogs(dog)))}
+const dogApi = ()=>{fetch('http://localhost:3000/dogs')
+.then (resp => resp.json())
+.then (json => json.forEach(dog => postDoggies(dog)))}
 
-const postDogs = (dog) =>{
+const postDoggies = (dog) =>{
 let tr = document.createElement('tr')
-let td = document.createElement('td')
-tr.dataset.id = dog.id
-tr.innerHTML = `<td data-name = ${dog.name}>${dog.name}</td> <td> ${dog.breed}</td> <td data-sex = ${dog.sex}>${dog.sex}</td> <td><button>Edit</button></td>`
-tableBody.append(tr)
-}
+tr.dataset.id =dog.id
+tr.innerHTML =`<td name="name">${dog.name}</td>
+<td name="breed">${dog.breed}</td> <td name="sex">${dog.sex}</td> <td><button data-id=${dog.id}>Edit</button></td>
+`
+tBody.append(tr)}
 document.addEventListener('click',function(e){
-  if(e.target.innerText == 'Edit'){
-   fetch(`http://localhost:3000/dogs/${e.target.parentElement.parentElement.dataset.id}`)
-   .then (response => response.json())
-   .then (json => form(json))
-  function form(json){ formBody.name.value = json.name,
-   formBody.breed.value=json.breed,
-   formBody.sex.value = json.sex
-  formBody.dataset.id = json.id}
-   document.addEventListener('submit',function(e){
-     e.preventDefault()
-     let form = e.target
-     fetch(`http://localhost:3000/dogs/${e.target.dataset.id}`, {
-      method: 'PATCH',
-      body: JSON.stringify({
-        name:`${e.target.name.value}`,
-        breed:`${e.target.breed.value}`,
-        sex:`${e.target.sex.value}`
-        }),
-        headers: {
-          "Content-type": "application/json"
-          }})
-     form.reset()
-   })}})
-fetchDogs()
+  if(e.target.textContent == "Edit"){
+    let tableValues = e.target.parentNode.parentNode.children
+   let name = tableValues.name.innerText
+   let breed = tableValues.breed.innerText
+   let sex = tableValues.sex.innerText
+  editForm.dataset.id= e.target.dataset.id
+   editForm.name.value =  name
+   editForm.breed.value =  breed
+   editForm.sex.value = sex
+  }
+})
+
+document.addEventListener('submit',function(e){
+  e.preventDefault()
+  let form =e.target
+  let name =form.name.value
+  let breed = form.breed.value
+  let sex = form.sex.value
+  fetch (`http://localhost:3000/dogs/${form.dataset.id}`,{
+    method :'PATCH',
+    body: JSON.stringify({
+      name: name,
+      breed : breed,
+      sex : sex
+    }),
+    headers: {
+      'content-type':'application/json'
+    }
+  })
+  let update = document.querySelector(`tr[data-id="${form.dataset.id}"]`)
+  update.children.name.innerText = name
+  update.children.breed.innerText = breed
+  update.children.sex.innerText = sex
+  editForm.reset()
+})
+dogApi()
 })
