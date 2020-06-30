d9a8155e9aa43cb4b731d3d4982dce414cf1bdec
github username <github email address>
2020-06-30 12:13:05 -0400

    done

--- src/index.js
+++ src/index.js
@@ -13,18 +13,32 @@ tr.dataset.id = dog.id
 tr.innerHTML = `<td data-name = ${dog.name}>${dog.name}</td> <td> ${dog.breed}</td> <td data-sex = ${dog.sex}>${dog.sex}</td> <td><button>Edit</button></td>`
 tableBody.append(tr)
 }
-
 document.addEventListener('click',function(e){
   if(e.target.innerText == 'Edit'){
-   console.log( e.target.parentElement.parentElement.datasetName)
-  //  formBody.name.value = 
-  //  formBody.breed.value=
-  //  formBody.sex.value =
-  }
-
-})
-
-
+   fetch(`http://localhost:3000/dogs/${e.target.parentElement.parentElement.dataset.id}`)
+   .then (response => response.json())
+   .then (json => form(json))
+  function form(json){ formBody.name.value = json.name,
+   formBody.breed.value=json.breed,
+   formBody.sex.value = json.sex
+  formBody.dataset.id = json.id}
+   document.addEventListener('submit',function(e){
+     let form = e.target
+     fetch(`http://localhost:3000/dogs/${e.target.dataset.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        name:`${e.target.name.value}`,
+        breed:`${e.target.breed.value}`,
+        sex:`${e.target.sex.value}`
+        }),
+        headers: {
+          "Content-type": "application/json"
+          }
+     })
+     form.reset()
+   })
+ 
+}})
 
 
 fetchDogs()
