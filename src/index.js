280bdcbf80f73d917722e13ee1d8517da801ba6e
github username <github email address>
2020-06-30 11:08:33 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
+let tableBody = document.getElementById("table-body")
+let formBody = document.getElementById('dog-form')
 
+const fetchDogs = () =>{fetch ("http://localhost:3000/dogs")
+.then(resp => resp.json())
+.then(json => json.forEach(dog => postDogs(dog)))}
+
+const postDogs = (dog) =>{
+let tr = document.createElement('tr')
+let td = document.createElement('td')
+tr.dataset.id = dog.id
+tr.innerHTML = `<td data-name = ${dog.name}>${dog.name}</td> <td> ${dog.breed}</td> <td data-sex = ${dog.sex}>${dog.sex}</td> <td><button>Edit</button></td>`
+tableBody.append(tr)
+}
+
+document.addEventListener('click',function(e){
+  if(e.target.innerText == 'Edit'){
+   console.log( e.target.parentElement.parentElement.datasetName)
+  //  formBody.name.value = 
+  //  formBody.breed.value=
+  //  formBody.sex.value =
+  }
+
+})
+
+
+
+
+fetchDogs()
 })
